42794f95013571a4d7683f534aba5249c7953ea2
Aditya Sharan <ranjan.aditya2009@gmail.com>
2021-07-02 01:46:51 +0530

    point 2 added v2

--- point2.java
+++ point2.java
@@ -1,21 +1,17 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.StringJoiner;
 
+//Input Hello How are you 
+//Output [Hello-How-are-you]
 public class Examplefeatures {
-	int length;
-	int breadth;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		List<Integer> numlist=Arrays.asList(5,4,3,2,1,3,3,3,4,5,6);
-		Map<Integer,Long> output =numlist.stream().collect
-				(
-				Collectors.groupingBy(Function.identity(),Collectors.counting())
-				);
-		output.forEach((k,v) -> {
-			System.out.println(k +" -" + v);
-		});
+		String input="Hello How are you";
+		String[] inputArray =input.split(" ");
+		StringJoiner joiner = new StringJoiner("-","[","]");
+		for(String inputArr : inputArray) {
+			joiner.add(inputArr);
+		}
+		System.out.println(joiner.toString());
+		
+		}
 	}
-}
