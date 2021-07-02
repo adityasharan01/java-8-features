b3174b18664c1fe7d143e869552d7f0b32bd2e52
Aditya Sharan <ranjan.aditya2009@gmail.com>
2021-07-02 12:22:54 +0530

    point 2 syntax added

--- point2.java
+++ point2.java
@@ -7,7 +7,7 @@ public class Examplefeatures {
 	public static void main(String[] args) {
 		String input="Hello How are you";
 		String[] inputArray =input.split(" ");
-		StringJoiner joiner = new StringJoiner("-","[","]");
+		StringJoiner joiner = new StringJoiner("-","[","]");  //arguments: delimiter,prefix,suffix
 		for(String inputArr : inputArray) {
 			joiner.add(inputArr);
 		}
