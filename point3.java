86d300ed3f84051d9bf8bfd8084bc1fe0c63c8c6
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-07-06 01:32:49 +0530

    Update point3.java

--- point3.java
+++ point3.java
@@ -4,3 +4,6 @@ Frequently asked interview programs on java 8 streams
 3.Write a program to print active and inactive employees in the given collection.
 4. Write a program to print Max/min employee salary from the given collection. 
 5.Write a program to print the max salary of an employee from each department. 
+
+  
+  
